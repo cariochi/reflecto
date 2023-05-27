3cf4b358466c77d1e0e81356dd90c89d69065d6f
Vadym Deineka <vdeineka@star.global>
2023-05-27 15:27:48 +0300

    v1.1.4

--- src/main/java/com/cariochi/reflecto/methods/JavaMethod.java
+++ src/main/java/com/cariochi/reflecto/methods/JavaMethod.java
@@ -5,10 +5,17 @@ import lombok.SneakyThrows;
 
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Method;
+import java.util.List;
+import java.util.Map;
 import java.util.Optional;
+import java.util.stream.Stream;
 
+import static java.util.Comparator.comparingInt;
+import static java.util.stream.Collectors.groupingBy;
+import static java.util.stream.IntStream.range;
+import static org.apache.commons.lang3.ClassUtils.getAllSuperclasses;
+import static org.apache.commons.lang3.ClassUtils.isAssignable;
 import static org.apache.commons.lang3.reflect.MethodUtils.getAnnotation;
-import static org.apache.commons.lang3.reflect.MethodUtils.getMatchingMethod;
 
 @RequiredArgsConstructor
 public class JavaMethod {
@@ -39,4 +46,27 @@ public class JavaMethod {
         return method.getReturnType();
     }
 
+    private static Method getMatchingMethod(final Class<?> cls, final String methodName, final Class<?>... parameterTypes) {
+        return Stream.concat(Stream.of(cls), getAllSuperclasses(cls).stream())
+                .flatMap(c -> Stream.of(c.getDeclaredMethods()))
+                .filter(m -> methodName.equals(m.getName()) && isAssignable(parameterTypes, m.getParameterTypes(), true))
+                .collect(groupingBy(m -> distance(parameterTypes, m.getParameterTypes()))).entrySet().stream()
+                .min(comparingInt(Map.Entry::getKey))
+                .map(Map.Entry::getValue).stream()
+                .flatMap(List::stream)
+                .findFirst()
+                .orElse(null);
+    }
+
+    private static int distance(final Class<?>[] fromClassArray, final Class<?>[] toClassArray) {
+        return range(0, fromClassArray.length)
+                .map(i -> {
+                    final Class<?> fromClass = fromClassArray[i];
+                    final Class<?> toClass = toClassArray[i];
+                    return fromClass == null || fromClass.equals(toClass)
+                            ? 0
+                            : (isAssignable(fromClass, toClass, true) && !isAssignable(fromClass, toClass, false) ? 1 : 2);
+                })
+                .sum();
+    }
 }
